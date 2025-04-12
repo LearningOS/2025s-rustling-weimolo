73e1d7349bb12aca50b8140298440ffdfe506c0c
weimolo <1466709263@qq.com>
2025-04-12 18:56:34 +0800

    f4-12-1

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -9,9 +9,9 @@
 enum Message {
     // TODO: define the different variants used below
     Quit,
-    Echo,
-    Move,
-    ChangeColor,
+    Echo(String),
+    Move { x: i32, y: i32 },
+    ChangeColor(i32, i32, i32),
 }
 
 impl Message {
