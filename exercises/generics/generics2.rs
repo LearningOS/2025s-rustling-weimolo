015ba350a8798b19068217d3399d8426c544d4ba
weimolo <1466709263@qq.com>
2025-05-15 18:20:47 +0800

    f5-15-2

--- exercises/generics/generics2.rs
+++ exercises/generics/generics2.rs
@@ -6,14 +6,19 @@
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-struct Wrapper {
-    value: u32,
+
+struct Wrapper<T> {
+    value: T,
 }
 
-impl Wrapper {
-    pub fn new(value: u32) -> Self {
+/*
+pub fn new(value: T) -> Wrapper<T> 定义了一个名为 new 的关联函数（使用 pub 关键字使其在结构体外部可访问）。关联函数是通过结构体类型直接调用的函数，而不是通过结构体实例调用。
+参数 value：该函数接受一个类型为 T 的参数 value。
+返回值：返回一个 Wrapper<T> 类型的实例。在函数体 Wrapper { value } 中，使用传入的 value 创建并返回一个新的 Wrapper 结构体实例，其中 value 字段被初始化为传入的 value。
+*/
+impl<T> Wrapper<T> {
+    pub fn new(value: T) -> Wrapper<T> {
         Wrapper { value }
     }
 }
@@ -30,5 +35,8 @@ mod tests {
     #[test]
     fn store_str_in_wrapper() {
         assert_eq!(Wrapper::new("Foo").value, "Foo");
+        //（使用 pub 关键字使其在结构体外部可访问）。关联函数是通过结构体类型直接调用的函数，而不是通过结构体实例调用。
     }
+
+    //Wrapper::new("Foo").value:这是结构体取value值，Wrapper::new("Foo")会返回一个Wrapper结构体，“.value”就是取值
 }
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,22 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
+
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        for i in 0..self.len() {
+            self[i] = format!("{}{}", self[i], "Bar");
+        }
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -23,7 +32,7 @@ mod tests {
     #[test]
     fn is_vec_pop_eq_bar() {
         let mut foo = vec![String::from("Foo")].append_bar();
-        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
-        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+        assert_eq!(foo.pop().unwrap(), String::from("FooBar"));
+        assert_eq!(foo.pop(), None);
     }
 }
